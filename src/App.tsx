6b4e542fb18ab27458b5552e61fae3b9f00bf4ed
Lyle Dippo <ldippo421@gmail.com>
2022-07-06 09:32:42 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { motion, useAnimationControls } from "framer-motion";
 import "./styles.css";
 
+// A red box with dimension 100x100
+// Box starts with a angle state of 0
+// Animation cycle is 1 second
+// Each second the base angle increases by 30deg
+// Animation includes scaleXY 1 -> 1.25 -> 1
+// Animation includes base - 30 -> base -> base + 30 -> base
+
 let baseAngle = 0;
 export default function App() {
   const controls = useAnimationControls();
@@ -9,9 +16,10 @@ export default function App() {
   React.useEffect(() => {
     const int = setInterval(() => {
       const angles = [baseAngle - 30, baseAngle, baseAngle + 30, baseAngle];
-
+      const scale = [1, 1.25, 1];
       controls.start({
         rotate: angles,
+        scale,
         transition: {
           duration: 1
         }
@@ -24,7 +32,7 @@ export default function App() {
   return (
     <motion.div
       animate={controls}
-      style={{ backgroundColor: "red", width: 100, height: 100 }}
+      style={{ backgroundColor: "red", width: 100, height: 100, margin: 200 }}
     />
   );
 }
